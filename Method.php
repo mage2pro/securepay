353e57755b2dcd926d04463197b94769d461c991
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-26 11:29:25 +0300

    no message

--- Method.php
+++ Method.php
@@ -1,4 +1,15 @@
 <?php
 // 2016-08-25
 namespace Dfe\SecurePay;
-class Method extends \Df\Payment\Method {}
+use Dfe\SecurePay\Settings as S;
+class Method extends \Df\Payment\Method {
+	/**
+	 * 2016-08-26
+	 * @return string
+	 */
+	private static function url() {
+		/** @var string $stage */
+		$stage = S::s()->test() ? 'test' : 'live';
+		return "https://api.securepay.com.au/{$stage}/directpost/authorise";
+	}
+}
--- Settings.php
+++ Settings.php
@@ -2,11 +2,50 @@
 // 2016-08-25
 namespace Dfe\SecurePay;
 /** @method static Settings s() */
-class Settings extends \Df\Payment\Settings\BankCard {
+final class Settings extends \Df\Payment\Settings\BankCard {
 	/**
 	 * 2016-08-26
 	 * «Mage2.PRO» → «Payment» → «SecurePay» → «Force the payment transactions result?»
 	 * @return bool
 	 */
 	public function forceResult() {return $this->v();}
+
+	/**
+	 * 2016-08-26
+	 * @return string
+	 */
+	public function merchantID() {return $this->testable();}
+
+	/**
+	 * 2016-08-26
+	 * @return string
+	 */
+	public function transactionPassword() {return $this->testable();}
+
+	/**
+	 * 2016-08-26
+	 * «Mage2.PRO» → «Payment» → «SecurePay» → «Live Merchant ID»
+	 * @return string
+	 */
+	protected function liveMerchantID() {return $this->v();}
+
+	/**
+	 * 2016-08-26
+	 * «Mage2.PRO» → «Payment» → «SecurePay» → «Live Transaction Password»
+	 * @return string
+	 */
+	protected function liveTransactionPassword() {return $this->p();}
+
+	/**
+	 * 2016-08-26
+	 * @return string
+	 */
+	protected function testMerchantID() {return $this->v();}
+
+	/**
+	 * 2016-08-26
+	 * We do not encrypt the test keys.
+	 * @return string
+	 */
+	protected function testTransactionPassword() {return $this->v();}
 }
