1f7952d8d4b54a163048e2ae5e18d33d9e1b878b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-27 19:19:14 +0300

    0.7.0

--- Signer.php
+++ Signer.php
@@ -2,38 +2,22 @@
 // 2016-08-26
 namespace Dfe\SecurePay;
 use Dfe\SecurePay\Settings as S;
-/**
- * 2016-08-26
- * «5.1.1.6 Fingerprint».
- * Mandatory
- * String, length up to 60
- * «A SHA1 hash of
- * 		For EPS_TXNTYPE 0-7:
- * 		EPS_MERCHANT|TransactionPassword|EPS_TXNTYPE|EPS_REFERENCEID|EPS_AMOUNT|EPS_TIMESTAMP
- *
- * 		For EPS_TXNTYPE 8:
- * 		EPS_MERCHANT|TransactionPassword|EPS_TXNTYPE|EPS_STORETYPE|EPS_REFERENCEID|EPS_TIMESTAMP
- * Where the EPS_ prefixed fields are sent in the request
- * and “TransactionPassword” is obtained from SecurePay Support
- * and maybe changed via the SecurePay Merchant Log In.»
- */
-final class Signer extends \Df\Payment\R\Signer {
+abstract class Signer extends \Df\Payment\R\Signer {
 	/**
-	 * 2016-08-26
+	 * 2016-08-27
+	 * @used-by \Dfe\SecurePay\Signer::sign()
+	 * @return string[]
+	 */
+	abstract protected function values();
+
+	/**
+	 * 2016-08-27
 	 * https://github.com/thephpleague/omnipay-securepay/blob/v2.1.0/src/Message/DirectPostAuthorizeRequest.php#L32-L47
 	 * @override
 	 * @see \Df\Payment\R\Signer::sign()
 	 * @return string
 	 */
-	public function sign() {return sha1(implode('|',
-		dfa_insert(
-			dfa_select_ordered($this->getData(), array_merge(
-				['EPS_MERCHANT', 'EPS_TXNTYPE'],
-				8 === $this['EPS_TXNTYPE']
-					? ['EPS_STORETYPE', 'REFERENCEID'] : ['EPS_REFERENCEID', 'EPS_AMOUNT'],
-				['EPS_TIMESTAMP']
-			)),
-			1, S::s()->transactionPassword())
-		)
-	);}
+	final protected function sign() {return sha1(implode('|',
+		dfa_insert($this->values(), 1, S::s()->transactionPassword())
+	));}
 }
---
+++ Signer/Response.php
@@ -0,0 +1,31 @@
+<?php
+// 2016-08-27
+namespace Dfe\SecurePay\Signer;
+use Dfe\SecurePay\Response as R;
+/** @method R caller() */
+final class Response extends \Dfe\SecurePay\Signer {
+	/**
+	 * 2016-08-27
+	 *
+	 * For EPS_TXNTYPE 0-7: merchant, transaction password, reference, amount, timestamp, summarycode
+	 * For EPS_TXNTYPE 8: merchant, transaction password, store type, reference, timestamp, summarycode
+	 *
+	 * @override
+	 * @see \Dfe\SecurePay\Signer::values()
+	 * @used-by \Dfe\SecurePay\Signer::sign()
+	 * @return string[]
+	 */
+	protected function values() {return array_merge(
+		[$this['merchant']],
+		8 === intval($this->req('EPS_TXNTYPE'))
+			? [$this->req('EPS_STORETYPE'), $this['refid']] : [$this['refid'], $this->req('EPS_AMOUNT')],
+		[$this['timestamp'], $this['summarycode']]
+	);}
+
+	/**
+	 * 2016-08-27
+	 * @param string $p
+	 * @return string|null
+	 */
+	private function req($p) {return $this->caller()->requestP($p);}
+}
