56bbd659a92814326605b46cb9a3c2fc47d9ee5e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-12 07:15:32 +0300

    1.1.4

--- Settings.php
+++ Settings.php
@@ -34,32 +34,5 @@ final class Settings extends \Df\Payment\Settings\BankCard {
 	 * 2016-08-26
 	 * @return string
 	 */
-	public function transactionPassword() {return $this->testable();}
-
-	/**
-	 * 2016-08-26
-	 * «Mage2.PRO» → «Payment» → «SecurePay» → «Live Merchant ID»
-	 * @return string
-	 */
-	protected function liveMerchantID() {return $this->v();}
-
-	/**
-	 * 2016-08-26
-	 * «Mage2.PRO» → «Payment» → «SecurePay» → «Live Transaction Password»
-	 * @return string
-	 */
-	protected function liveTransactionPassword() {return $this->p();}
-
-	/**
-	 * 2016-08-26
-	 * @return string
-	 */
-	protected function testMerchantID() {return $this->v();}
-
-	/**
-	 * 2016-08-26
-	 * We do not encrypt the test keys.
-	 * @return string
-	 */
-	protected function testTransactionPassword() {return $this->v();}
+	public function transactionPassword() {return $this->testablePV();}
 }
