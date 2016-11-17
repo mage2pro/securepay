e202926ecc08516035f22883add534234d49c789
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-17 10:05:51 +0300

    1.1.5

--- Block/Info.php
+++ Block/Info.php
@@ -2,25 +2,16 @@
 // 2016-08-28
 namespace Dfe\SecurePay\Block;
 use Dfe\SecurePay\Response as R;
-use Magento\Framework\DataObject;
 /** @method R|string|null responseF(string $key = null) */
 class Info extends \Df\Payment\R\BlockInfo {
 	/**
 	 * 2016-08-28
 	 * @override
-	 * @see \Magento\Payment\Block\ConfigurableInfo::_prepareSpecificInformation()
-	 * @used-by \Magento\Payment\Block\Info::getSpecificInformation()
-	 * @param DataObject|null $transport
-	 * @return DataObject
+	 * @see \Df\Payment\Block\Info::prepare()
+	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
 	 */
-	protected function _prepareSpecificInformation($transport = null) {
-		/** @var DataObject $result */
-		$result = parent::_prepareSpecificInformation($transport);
-		$result['Card Number'] = str_replace('...', '*******', $this->responseF('pan'));
-		if ($this->isBackend()) {
-			$result['SecurePay ID'] = $this->responseF('txnid');
-		}
-		$this->markTestMode($result);
-		return $result;
+	protected function prepare() {
+		$this->si('Card Number', str_replace('...', '*******', $this->responseF('pan')));
+		$this->siB('SecurePay ID', $this->responseF('txnid'));
 	}
 }
