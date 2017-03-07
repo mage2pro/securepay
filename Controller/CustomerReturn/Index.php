c67bb9f98b1c26bf3b7b57d8d599b219e14cdd73
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-07 11:03:46 +0300

    no message

--- Controller/CustomerReturn/Index.php
+++ Controller/CustomerReturn/Index.php
@@ -1,4 +1,5 @@
 <?php
-// 2016-08-27
 namespace Dfe\SecurePay\Controller\CustomerReturn;
+// 2016-08-27
+/** @final Unable to use the PHP «final» keyword because of the M2 code generation. */
 class Index extends \Df\Payment\Action\CustomerReturn {}
