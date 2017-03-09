000db702cdd869478772855b3f0ff8eb441e20f3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 07:19:53 +0300

    no message

--- Block/Info.php
+++ Block/Info.php
@@ -1,10 +1,9 @@
 <?php
 namespace Dfe\SecurePay\Block;
-use Dfe\SecurePay\Webhook as W;
 /**
  * 2016-08-28
  * @final Unable to use the PHP «final» keyword because of the M2 code generation.
- * @method W|string|null responseF(string $k = null)
+ * @method string|null responseF(string $k = null)
  */
 class Info extends \Df\PaypalClone\BlockInfo {
 	/**
