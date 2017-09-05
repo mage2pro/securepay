a18e576ef5ca799b0a9f157555667e46f16ffe58
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:38:50 +0300

    1.5.22

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,7 +1,9 @@
 // 2016-08-25
 define([
 	'df', 'Df_Core/my/redirectWithPost', 'Df_Payment/card'
-], function(df, redirectWithPost, parent) {'use strict'; return parent.extend({
+], function(df, redirectWithPost, parent) {'use strict';
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */	
+return parent.extend({
 	/**
 	 * 2016-08-26
 	 * «3.6.2 Simulating Approved and Declined Transactions
