76364a6096329995bc179d75e92c3a5cb2e1cde3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-16 02:04:54 +0300

    no message

--- Url.php
+++ Url.php
@@ -3,7 +3,9 @@ namespace Dfe\SecurePay;
 // 2017-03-23
 final class Url extends \Df\Payment\Url {
 	/**
-	 * 2016-08-27 Первый параметр — для test, второй — для live.
+	 * 2016-08-27
+	 * The method returns a 2-tuple:
+	 * the first element is for the test mode, the second is for the production mode.
 	 * 2017-02-16 SecurePay has changed the URL for Direct Post API testing: https://mage2.pro/t/2779
 	 * @override
 	 * @see \Df\Payment\Url::stageNames()
