95aa3fa7726a4c4123ce05d9935179f9c4dce8a1
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-29 02:18:06 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -57,10 +57,11 @@ return parent.extend({
 		return result;
 	}),
 	/**
-	 * 2016-08-25
-	 * Which bank card networks does SecurePay support? https://mage2.pro/t/1989
-	 * 2017-02-05
-	 * The bank card network codes: https://mage2.pro/t/2647
+	 * 2016-08-25 Which bank card networks does SecurePay support? https://mage2.pro/t/1989
+	 * 2017-02-05 The bank card network codes: https://mage2.pro/t/2647
+	 * @override
+	 * @see Df_Payment/main::getCardTypes()
+	 * @used-by https://github.com/mage2pro/core/blob/3.9.12/Payment/view/frontend/web/template/card/fields.html#L4
 	 * @returns {String[]}
 	 */
 	getCardTypes: function() {return ['VI', 'MC'];},
