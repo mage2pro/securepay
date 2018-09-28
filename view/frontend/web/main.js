c0c9015d25c2eda662f08f11178aa459bba68baa
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-29 05:28:15 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,7 +1,5 @@
 // 2016-08-25
-define([
-	'df', 'Df_Core/my/redirectWithPost', 'Df_Payment/card'
-], function(df, redirectWithPost, parent) {'use strict';
+define(['df', 'Df_Payment/card'], function(df, parent) {'use strict';
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */	
 return parent.extend({
 	/**
